71d04921e6fb2217be0f82fcb8df53d9b5ce4c69
ivancastanop <107499323+ivancastanop@users.noreply.github.com>
2023-05-25 21:38:56 -0500

    Add files via upload

---
+++ Formateador.py
@@ -0,0 +1,95 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+from io import BytesIO
+
+# Lista de nombres de columnas restantes en el orden deseado
+columnas_restantes = [
+    'CÓDIGO SECTOR GENERAL',
+    'CÓDIGO SECTOR ESPECÍFICO',
+    'ENSAYO',
+    'TÉCNICA',
+    'SUSTANCIA, MATERIAL, ELEMENTO O PRODUCTO A ENSAYAR',
+    'INTERVALO DE MEDICIÓN',
+    'DOCUMENTO NORMATIVO',
+    'ACTIVIDAD DE ASEGURAMIENTO',
+    'ACTIVIDAD DE ASEGURAMIENTO',
+    'ACTIVIDAD DE ASEGURAMIENTO',
+    'ACEPTACIÓN DE LA JUSTIFICACIÓN'
+]
+
+def procesar_archivos(archivos):
+    consolidado_df = pd.DataFrame()
+    for archivo in archivos:
+        #start_row=0
+        #end_row=0
+        #if archivo.endswith('.xlsx') or archivo.endswith('.xls'):
+        # Lee el archivo Excel
+        #st.write(archivo)
+        df = pd.read_excel(archivo, sheet_name='2019-05-09')
+
+        # Encuentra la fila que contiene el texto "CÓDIGO SECTOR GENERAL" en la primera columna y empieza a extraer la información a partir de la siguiente fila
+        start_row = df[df.iloc[:, 1] == 'CÓDIGO SECTOR GENERAL'].index[0] + 1
+        print(start_row)
+
+        # Encuentra la última fila donde la tercera columna no tiene datos
+        end_row = 21  # Inicialmente, establece end_row en 20 filas después del start_row
+
+        # Itera desde la fila 24 hasta el final del DataFrame
+        for row in range(21, len(df)):
+            if pd.isnull(df.iloc[row, 6]):
+                end_row = row  # Actualiza end_row cuando se encuentra una fila con la primera columna vacía
+                break
+        print(end_row)
+
+        filtered_df = df.iloc[start_row:end_row, 1:]
+        filtered_df.reset_index(drop=True, inplace=True)
+
+        # Extrae la fecha de la columna 3, fila 7
+        date_str = str(df.iloc[5, 2])
+        date = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')  # Convierte la cadena de fecha en un objeto datetime
+
+        # Agrega la fecha extraída como una nueva columna en el DataFrame filtrado
+        filtered_df['Fecha'] = date
+
+        # Extrae el año de la fecha y crea una nueva columna en el DataFrame filtrado
+        filtered_df['Año'] = date.year
+
+        # Extrae el Codigo de Acreditación y crea una nueva columna en el DataFrame filtrado
+        filtered_df['Codigo de Acreditacion']=df.iloc[13, 5]
+
+        # Extrae el nombre del OEC y crea una nueva columna en el DataFrame filtrado
+        filtered_df['OEC']=df.iloc[7, 2]
+
+        # Se origaniza el DataFrame para que tenga el orden adecuado
+        filtered_df.insert(0, 'Año', filtered_df.pop('Año'))
+        filtered_df.insert(0, 'Fecha', filtered_df.pop('Fecha'))
+        filtered_df.insert(0, 'Codigo de Acreditacion', filtered_df.pop('Codigo de Acreditacion'))
+        filtered_df.insert(0, 'OEC', filtered_df.pop('OEC'))
+
+        # Se le da el nombre a las columnas
+        filtered_df.columns = ['OEC', 'Codigo de Acreditacion', 'Fecha', 'Año'] + columnas_restantes
+
+        # Agrega los datos filtrados al DataFrame principal
+        consolidado_df = pd.concat([consolidado_df, filtered_df], ignore_index=True)
+    #st.write("Termine la funcion")
+    consolidado_df.to_excel('ConsolidadoStreamlit.xlsx', index=False)        
+    return consolidado_df
+
+st.title('Aplicación para formatear archivos de JNP en ONAC')
+
+uploaded_files = st.file_uploader("Selecciona los archivos a cargar: ", accept_multiple_files=True)
+#st.write(uploaded_files)
+
+# Botón para procesar los archivos
+if st.button('Procesar archivos'):
+    st.info('Procesando archivos... Esto puede llevar un tiempo.')
+    archivo_resultado = procesar_archivos(uploaded_files)
+    st.info('Proceso terminado con exito')
+
+    with open('ConsolidadoStreamlit.xlsx', "rb") as template_file:
+        template_byte = template_file.read()
+        st.download_button(label='📥 Descargar el Resultado',
+                        data=template_byte,
+                        file_name="ArchivoFormateado.xlsx",
+                        mime='application/octet-stream')
